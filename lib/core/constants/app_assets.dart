604c68f43ac98bac147462e4dae27b5c5ca75449
Usama Elgendy <usamaaelgendy@gmail.com>
2025-10-07 19:07:19 +0300

    fix: correct filename case for triangle arrow asset to ensure proper loading

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -8,7 +8,7 @@ class AppAssets {
   static const String onboardingMobile = "$_path/onboarding_mobile.svg";
   static const String onboardingRecycling = "$_path/onboarding_recycling.svg";
   static const String picture = "$_path/picture.svg";
-  static const String triangleArrow = "$_path/Triangle_Arrow.svg";
+  static const String triangleArrow = "$_path/triangle_arrow.svg";
 
   static const String firstOnboard = '$_path/illustation.png';
   static const String secondOnboard = '$_path/illustation (1).png';
