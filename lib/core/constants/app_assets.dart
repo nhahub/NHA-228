844428d93f9e6555763a424cd99a2b411936d55a
Usama Elgendy <usamaaelgendy@gmail.com>
2025-10-07 19:04:52 +0300

    fix: correct filename case for calendar asset to ensure proper loading

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -2,7 +2,7 @@ class AppAssets {
   static const String _path = "assets/images";
   static const String logo = "$_path/logo.png";
   static const String splash = "$_path/splash.png";
-  static const String calendar = "$_path/Calendar.svg";
+  static const String calendar = "$_path/calendar.svg";
   static const String camera = "$_path/camera.svg";
   static const String onboardingLocation = "$_path/onboarding_location.svg";
   static const String onboardingMobile = "$_path/onboarding_mobile.svg";
