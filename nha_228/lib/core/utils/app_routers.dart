3e1c7458547f2c870390049718f9f8df14b082bb
reham <najehreham9@gmail.com>
2025-10-07 13:12:55 +0300

    add auth service

--- nha_228/lib/core/utils/app_routers.dart
+++ nha_228/lib/core/utils/app_routers.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:nha_228/core/services/auth_service.dart';
 import 'package:nha_228/features/auth/screens/login_screen.dart';
 import 'package:nha_228/features/auth/screens/register_screen.dart';
 import 'package:nha_228/features/home/screens/home_screen.dart';
@@ -6,6 +7,7 @@ import 'package:nha_228/features/home/screens/search_screen.dart';
 import 'package:nha_228/features/home/widgets/custom_vav_bar.dart';
 import 'package:nha_228/features/post/screens/new_post_screen.dart';
 import 'package:nha_228/features/splash/screens/onboard_screen.dart';
+import 'package:nha_228/features/splash/screens/splash_screen.dart';
 
 abstract class AppRouter {
   static const homeScreen = '/HomeScreen';
@@ -17,7 +19,17 @@ abstract class AppRouter {
 
   static final router = GoRouter(
     routes: [
-      GoRoute(path: '/', builder: (context, state) => CustomNavBar()),
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          final isLoggedIn = AuthService.isUserLoggedIn();
+          if (isLoggedIn) {
+            return const CustomNavBar();
+          }else {
+            return const SplashScreen();
+          }
+        },
+      ),
       GoRoute(path: loginScreen, builder: (context, state) => LoginScreen()),
       GoRoute(
         path: onboardScreen,
@@ -36,8 +48,10 @@ abstract class AppRouter {
       GoRoute(
         path: searchScreen,
         builder: (context, state) {
-          final String searchWord=state.extra as String;
-          return SearchScreen(searchWord: searchWord);}) ,
+          final String searchWord = state.extra as String;
+          return SearchScreen(searchWord: searchWord);
+        },
+      ),
       GoRoute(path: homeScreen, builder: (context, state) => CustomNavBar()),
       GoRoute(
         path: newPostScreen,
