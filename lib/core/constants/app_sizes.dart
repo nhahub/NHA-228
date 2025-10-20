cfe83839ddcac4edeac1d778b7b79b62e32054ae
MahmoudElhenawy <mahmoud202203355@gmail.com>
2025-10-20 21:38:06 +0300

    Save local changes before pulling

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -16,11 +16,10 @@ class AppSizes {
   static double h44 = 44.h;
   static double h48 = 48.h;
   static double h60 = 60.h;
-  static double h44 = 44.h;
   static double h50 = 50.h;
   static double h54 = 54.h;
   static double h70 = 70.h;
-  static double h150 = h150;
+  static double h150 = 150.h;
   static double h200 = 200.h;
 
   // Font Size
@@ -28,10 +27,8 @@ class AppSizes {
   static double sp16 = 16.sp;
   static double sp18 = 18.sp;
   static double sp20 = 20.sp;
-  static double sp22 = 22.sp;
   static double sp24 = 24.sp;
   static double sp26 = 26.sp;
-  static double sp16 = 16.sp;
   static double sp22 = 22.sp;
   static double sp32 = 32.sp;
   static double sp38 = 38.sp;
