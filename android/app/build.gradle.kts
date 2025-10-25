5694f5da011ae4c78acd29d6246f5eb8d572d578
reham <najehreham9@gmail.com>
2025-10-25 13:37:35 +0300

    upgrade plugin versions in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "com.example.nha_228"
 
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
 
         versionCode = flutter.versionCode.toInt() 
