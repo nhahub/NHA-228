fe64aa9caefa916a0e112d7cb5d11933a3ba2e7f
reham <najehreham9@gmail.com>
2025-10-28 15:56:42 +0300

    remove unused import

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nha_228/core/constants/app_colors.dart';
 import 'package:nha_228/core/constants/app_sizes.dart';
 
